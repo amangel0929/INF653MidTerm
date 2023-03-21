43c75defa31497834cb7b83a3c3a426d976a20e6
Alexis <alexis13oo@cox.com>
2023-03-20 22:05:16 -0400

    header changes

--- api/categories/update.php
+++ api/categories/update.php
@@ -1,10 +1,4 @@
 <?php
-    //Headers
-    header('Access-Control-Allow-Origin:*');
-    header('Content-Type:application/json');
-    header('Access-Control-Allow-Origin:PUT');
-    header('Access-Control-Allow-Origin:Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
-
     include_once '../../config/Database.php';
     include_once '../../models/Category.php';
 
