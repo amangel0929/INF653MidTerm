44862e4422d9bfabffc737700eb071a997a459e7
Alexis <alexis13oo@cox.com>
2023-03-22 20:00:22 -0400

    quotes update fixed

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -1,33 +1,32 @@
 <?php
 
-    include_once '../../config/Database.php';
-    include_once '../../models/Quote.php';
+include_once '../../config/Database.php';
+include_once '../../models/Quote.php';
 
-    $database = new Database();
-    $db = $database->connect();
+$database = new Database();
+$db = $database->connect();
 
-    $quote = new Quote($db);
+$quote = new Quote($db);
 
-    //Get raw posted data
-    $data = json_decode(file_get_contents('php://input'));
+//Get raw posted data
+$data = json_decode(file_get_contents('php://input'));
 
-    //Set ID to update
-    $quote->id = $data->id;
+//Set ID to update
+$quote->id = $data->id;
+$quote->quote = $data->quote;
+$quote->author_id = $data->author_id;
+$quote->category_id = $data->category_id;
 
-    $quote->quote = $data->quote;
-    $quote->author_id = $data->author_id;
-    $quote->category_id = $data->category_id;
-
-    if($quote->update()) {
-        $result = $quote_arr = array(
-            'id' => $quote->id,
-            'quote' => $quote->quote,
-            'author_id' => $quote->author_id,
-            'category_id' => $quote->category_id
-        );
-        echo json_encode($result);
-    } else {
-        $message = array("message" => "No Quotes Found");
-        echo json_encode($message);
-    }
+if($quote->update()){
+    $quote_arr = array(
+        'id' => $quote->id,
+        'quote' => $quote->quote,
+        'author_id' => $quote->author_id,
+        'category_id' => $quote->category_id
+    );
+    echo json_encode($quote_arr);
+}else{
+    $message = array("message" => "Missing Required Parameters");
+    echo json_encode($message);
+}
 ?>
